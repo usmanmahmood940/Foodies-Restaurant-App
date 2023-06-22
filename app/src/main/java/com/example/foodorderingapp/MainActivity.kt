a5aaa048ad769786280ca7cb583aaf18686fada5
Usman <usman.mahmood@brainxtech.com>
2023-06-22 19:37:50 +0500

    Splash Screen + Half Home Screen

---
+++ app/src/main/java/com/example/foodorderingapp/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.foodorderingapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.foodorderingapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnStart.setOnClickListener {
+            val intent = Intent(this,HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
